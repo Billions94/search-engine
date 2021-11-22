87b8e849fdfc1905b2c87be7af1bda52c8ea241c
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2021-11-22 20:55:59 +0100

    some styling

--- src/components/Home/Jobs.jsx
+++ src/components/Home/Jobs.jsx
@@ -5,15 +5,33 @@ const Jobs = ({ data }) => {
     console.log("mind you i'm in jobs",data)
   return (
     <div className='mt-5 customDiv text-dark pl-2'>
-        <h3 className='text-dark text-left mb-5'>Job offers</h3>
+        <h3 className='text-light text-left mb-5'>Jobs </h3>
       {data && data.map(d => (
-        <div key={d._id} className='customBottomBorder ListGroup'>
-            <Link key={d._id}  className='text-dark' to={`/${d.company_name}`} >
-                <h6 className='text-left'>Job Title: {d.title}</h6>
-            </Link>
-                <h6 className='text-left'>Company: {d.company_name}</h6>
-                <h6 className='text-left'>Job Type: {d.job_type}</h6>
-        </div>
+        <ul key={d._id} className=' list-group '>
+            <li className='text-left list-group-item pl-3'>
+                <div className='d-flex'>
+                    <div className='mr-2'>
+                    <img className='roundpic' src='https://picsum.photos/200/300' alt='' width='50px' />
+                    </div>
+                    <div>
+                    <Link key={d._id}  className='text-dark' to={`/${d.company_name}`} >
+                        <div className=''>
+                            <div> <span className='customerh1Span'>Job Title: {d.title}</span> </div>
+                        </div>
+                    </Link>
+                    <div className='customerh3Span text-muted'>
+                        Company: {d.company_name}
+                    </div>
+                    <div className='customerh3Span text-muted'>
+                        Job Type: {d.job_type}
+                    </div>
+                    <div className='customerh3Span text-muted'>
+                        Location: {d.candidate_required_location}
+                    </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
       ))}
     </div>
   );
