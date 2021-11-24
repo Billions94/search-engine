53c82e489ce54c45245111c82462da18f77eb428
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2021-11-24 16:59:00 +0100

    added indicator

--- src/components/Favorites/Favorite.jsx
+++ src/components/Favorites/Favorite.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeFromFavAction } from "../../redux/actions";
 import { Link } from "react-router-dom"
+import "./styles.css"
 
 const mapStateToProps = (state) => ({
   favorite: state.data.favorites,
@@ -16,15 +17,18 @@ const mapDispatchToProps = (dispatch) => ({
 const Favorite = ({ favorite, removeFromFavorites }) => {
   console.log("favorite", favorite);
   return (
-    <div>
+    <div className='favDivContainer mx-auto col-6 mt-3 '>
+        
+        <h1 className='text-light text-left favList sticky-top'> Favorite List </h1>
+        <h1 className='text-light indicator text-right'>{favorite.length}</h1>
       {favorite &&
         favorite.map((el, i) => (
-          <div key={el._id} className='d-flex justify-content-center mt-4'>
-                <Link to={`/${el.company_name}`}>
-                 <h6 className="text-light">{el.company_name}</h6>
+          <div key={el._id} className='d-flex justify-content-center mt-4 customh1'>
+                <Link  to={`/${el.company_name}`} className='customh1'>
+                 <h1 className="customh1 text-left">{el.company_name}</h1>
                 </Link>
-               <div className='ml-4'>
-                 <img onClick={() => removeFromFavorites(i)} src="https://img.icons8.com/fluency/50/000000/delete-forever.png" width='20px'/>
+               <div className='ml-4 removeFav'>
+                 <img onClick={() => removeFromFavorites(i)} src="https://img.icons8.com/fluency/50/000000/delete-sign.png" width='20px'/>
                </div>  
           </div>
         ))}
