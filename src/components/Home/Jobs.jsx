e37f1838b730a6d31e392c276c038c928ed741b4
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2021-11-24 19:08:44 +0100

    outline added

--- src/components/Home/Jobs.jsx
+++ src/components/Home/Jobs.jsx
@@ -1,54 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { addToFavAction } from "../../redux/actions";
+import { addToFavAction, removeFromFavAction } from "../../redux/actions";
+import SingleJob from "./SingleJob";
 
-const mapStateToProps = state => ({})
-
-const mapDispatchToProps = (dispatch) => ({
-    addToFavorites: (fav) => {
-      dispatch(addToFavAction(fav))
-    }
+const mapStateToProps = state => ({
+  favorite: state.data.favorites,
 })
 
-const Jobs = ({ data, addToFavorites } ) => {
+const mapDispatchToProps = (dispatch) => ({
+  addToFavorites: (fav) => {
+    dispatch(addToFavAction(fav))
+  },
+  removeFromFavorites: (favIndex) => {
+    dispatch(removeFromFavAction(favIndex))
+  }
+})
+
+
+const Jobs = ({ data, favorite, addToFavorites, removeFromFavorites } ) => {
+
+  
+
   console.log("mind you i'm in data",data)
     console.log("mind you i'm in jobs",addToFavorites)
+
+ 
+
+
   return (
     <div className='mt-5 mb-0 customDiv text-dark px-3 pt-0 pb-2'>
-        <h3 className='text-light mt-0 text-left mb-5 jobs sticky-top'>Jobs </h3>
-        
-      {data && data.map(d => (
-        <ul key={d._id} className=' list-group '>
-            <div className='text-left NaN list-group-item pl-3 mb-2'>
-                <div className='jobDivWrapper d-flex'>
-                  <div className='d-flex'>
-                    <div className='mr-2'>
-                    <img className='roundpic' src='https://picsum.photos/200/300' alt='' width='50px' />
-                    </div>
-                    <div className=''>
-                      <Link key={d._id}  className='text-dark' to={`/${d.company_name}`} >
-                          <div className='customerh1Span'>
-                            Job Title: {d.title}
-                          </div>
-                      </Link>
-                      <div className='customerh3Span text-muted'>
-                          Company: {d.company_name}
-                      </div>
-                      <div className='customerh3Span text-muted'>
-                          Job Type: {d.job_type}
-                      </div>
-                      <div className='customerh3Span text-muted'>
-                          Location: {d.candidate_required_location}
-                      </div>
-                    </div>
-                </div>    
-                    <div className='favoriteIcon text-muted ml-4'>
-                        <img onClick={() => addToFavorites(d)} src="https://img.icons8.com/color/50/000000/add-to-favorites.png" width='30px'/>
-                    </div>
-                </div>
-            </div>
-        </ul>
+        <h3 className='text-light mt-0 text-left mb-5 jobs sticky-top'>Jobs </h3> 
+      {data && data.map((d, i)=> (
+        <SingleJob 
+        addToFavorites={addToFavorites}
+        favorite={favorite}
+        removeFromFavorites={removeFromFavorites}
+         d={d} i={i}/>
       ))}
     </div>
   );
---
+++ src/components/Home/SingleJob.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+
+const SingleJob  = ({ data, favorite, addToFavorites, removeFromFavorites, d, i}) => {
+    const [selected, setSelected] = useState(false)
+
+    const multiTask = (d) => {
+        setSelected(true)
+        addToFavorites(d)
+      }
+  
+      const removeFavorite = (i) => {
+        setSelected(false)
+        removeFromFavorites(i)
+      }
+  
+      useEffect(() => {
+        if(favorite.indexOf(d) !== -1){
+            setSelected(true)
+        }else(setSelected(false))
+        console.log('this id the company name ', d.company_name)
+    },[])
+
+    return(
+        <div>
+            <ul key={i} className=' list-group '>
+                <div className='text-left NaN list-group-item pl-3 mb-2'>
+                    <div className='jobDivWrapper d-flex'>
+                      <div className='d-flex'>
+                        <div className='mr-2'>
+                        <img className='roundpic' src='https://picsum.photos/200/300' alt='' width='50px' />
+                        </div>
+                        <div className=''>
+                          <Link key={d._id}  className='text-dark' to={`/${d.company_name}`} >
+                              <div className='customerh1Span'>
+                                Job Title: {d.title}
+                              </div>
+                          </Link>
+                          <div className='customerh3Span text-muted'>
+                              Company: {d.company_name}
+                          </div>
+                          <div className='customerh3Span text-muted'>
+                              Job Type: {d.job_type}
+                          </div>
+                          <div className='customerh3Span text-muted'>
+                              Location: {d.candidate_required_location}
+                          </div>
+                        </div>
+                    </div>    
+                        <div className='favoriteIcon text-muted ml-4'>
+                          {
+                            selected === false ? 
+                            <img onClick={() => multiTask(d)} src="https://img.icons8.com/color/50/000000/add-to-favorites.png" width='30px'/>
+                          : <img onClick={() => removeFavorite(i)} src="https://img.icons8.com/ultraviolet/50/000000/add-to-favorites.png" width='30px'/>
+                          }
+                            
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    )
+}
+
+
+export default SingleJob
